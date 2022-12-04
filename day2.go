c31621e87f8f6a3b4826e8fd8d18ef8c83cdefae
Roy de Wildt <roydewildt@gmail.com>
2022-12-04 22:21:46 +0100

    Solved day 2 part 2

--- day2.go
+++ day2.go
@@ -10,12 +10,6 @@ type Day2 struct {
 }
 
 const (
-  rock = iota
-  paper
-  scissors
-)
-
-const (
   lose = 0
   draw = 3
   win = 6
@@ -34,26 +28,58 @@ func(d Day2) Solve() {
     'C': "scissors",
   }
 
+  res := map[byte]int {
+    'X': lose,
+    'Y': draw,
+    'Z': win,
+  }
 
   buf := bufio.NewScanner(d.PuzzleInput)
   defer d.PuzzleInput.Close()
 
   var totalScore int
+  var totalScoreAlt int
 
   for buf.Scan() {
     l := buf.Text()
     
     elfMove := elf[l[0]]
     meMove := me[l[2]]
-
-
     score := gameOutcome(meMove, elfMove) + movePoints(meMove)
     totalScore += score
     log.Printf("My move %s, elf move %s (score %d, total %d)\n", meMove, elfMove, score, totalScore)
+
+    outcome := res[l[2]]
+    meMoveAlt := inferMove(elfMove, outcome)
+    scoreAlt := gameOutcome(meMoveAlt, elfMove) + movePoints(meMoveAlt)
+    totalScoreAlt += scoreAlt
+    log.Printf("Elf move %s, required outcome %d, my move %s (score %d, total %d)\n", elfMove, outcome, meMoveAlt, scoreAlt, totalScoreAlt)
+
   }
 
-  log.Printf("Answer Part I: %d", totalScore)
+  log.Printf("Answer Part II: %d", totalScoreAlt)
   
+}
+
+func inferMove(opponentMove string, outcome int) string {
+  switch {
+  case outcome == draw:
+    return opponentMove
+  case opponentMove == "rock" && outcome == win:
+    return "paper"
+  case opponentMove == "paper" && outcome == win:
+    return "scissors"
+  case opponentMove == "scissors" && outcome == win:
+    return "rock"
+  case opponentMove == "rock" && outcome == lose:
+    return "scissors"
+  case opponentMove == "paper" && outcome == lose:
+    return "rock"
+  case opponentMove == "scissors" && outcome == lose:
+    return "paper"
+  default:
+    return ""
+  }
 }
 
 func gameOutcome(yourMove, opponentMove string) int {
@@ -70,6 +96,8 @@ func gameOutcome(yourMove, opponentMove string) int {
     return lose
   }
 }
+
+
 
 func movePoints(move string) int {
   switch move {
